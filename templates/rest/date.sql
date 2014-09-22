6a85a34f130eaa51f2dd5c09e339c37321349a7c
chlr <chlr@groupon.com>
2014-09-22 22:12:43 +0530

    date query has been changed

--- templates/rest/date.sql
+++ templates/rest/date.sql
@@ -1,23 +1,36 @@
-select 
-sys_class_name,
-task_id,
-name,
-summary,
-invoked_by,
-DATE_FORMAT(queued_time,'%Y-%m-%d') as queued_time,
-DATE_FORMAT(start_time,'%Y-%m-%d') as start_time,
-DATE_FORMAT(end_time,'%Y-%m-%d') as end_time,
-task_ref_count,
-status_code,
-agent,
-workflow_id,
-trigger_id,
-duration_seconds,
-retry_maximum,
-retry_counter,
-retry_indefinitely,
-retry_interval,
-attempt_count
-from ops_exec where cast(queued_time as date)= '{{run_date}}'
-or cast(start_time as date) = '{{run_date}}'
-or cast(end_time as date) = '{{run_date}}';
+SELECT
+t.ls_enabled,
+t.name,
+t.retry_indefinitely,
+t.retry_interval,
+t.retry_maximum,
+t.summary,
+t.sys_class_name,
+DATE_FORMAT(t.sys_created_on,'%Y-%m-%d %H:%i:%s') as task_created_by,
+t.sys_created_by,
+t.sys_id,
+t.sys_updated_by,
+DATE_FORMAT(t.sys_updated_on,'%Y-%m-%d %H:%i:%s') as task_updated_on,
+DATE_FORMAT(e.workflow_start_time,'%Y-%m-%d %H:%i:%s') as workflow_start_time,
+e.workflow_id,
+e.trigger_id,
+e.task_ref_count,
+e.status_description,
+e.status_code,
+DATE_FORMAT(e.start_time,'%Y-%m-%d %H:%i:%s') as start_time,
+e.start_held_reason,
+DATE_FORMAT(e.queued_time,'%Y-%m-%d %H:%i:%s') as queued_time,
+e.invoked_by,
+e.execution_user,
+DATE_FORMAT(e.end_time,'%Y-%m-%d %H:%i:%s') as end_time,
+e.duration_seconds,
+e.duration,
+e.attempt_count,
+e.agent
+FROM 
+ops_task t 
+LEFT OUTER JOIN
+ops_exec e
+ON t.sys_id = e.task_id
+WHERE cast(e.sys_updated_on as date) = {{run_date}}
+or cast(e.sys_updated_on as date) = {{run_date}};
